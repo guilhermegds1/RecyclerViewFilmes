4cb2f33c0bcde13f0f49c6c394db45fe8ee777d1
AlisonDuarte <dru.rodrigo@outlook.com>
2023-03-14 21:30:23 -0300

    criando adaptador

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
@@ -1,4 +1,55 @@
 package br.com.etecia.recyclerviewfilmes;
 
+
+
 public class Filmes {
+    private String Tutulo;
+    private String decricao;
+    private String categoria;
+    private int imagem;
+
+    //criar construtor com parametros alt+insert
+
+
+    public Filmes(String tutulo, String decricao, String categoria, int imagem) {
+        Tutulo = tutulo;
+        this.decricao = decricao;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+
+       //criando os metodos de acesso get and setter
+
+
+    public String getTutulo() {
+        return Tutulo;
+    }
+
+    public void setTutulo(String tutulo) {
+        Tutulo = tutulo;
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public void setDecricao(String decricao) {
+        this.decricao = decricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
